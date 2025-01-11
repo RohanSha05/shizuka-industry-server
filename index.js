9e864a9213a27ba208b828238fd01409bbc92248
RohanSha05 <rhnshariar@gmail.com>
2025-01-11 21:59:59 +0600

    updated code3

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 app.use(cors());
 app.use(express.json());
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kgl7e.mongodb.net/?retryWrites=true&w=majority`;
 console.log(process.env.DB_PASS);
 console.log(uri);
